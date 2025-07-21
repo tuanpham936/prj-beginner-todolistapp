415e63bf6cd8c5f531c255105c78932584bae527
tuanpham936 <tuan.phamminh936@gmail.com>
2025-07-21 17:27:04 +0700

    Project Initial commit

---
+++ src/test/java/com/projects/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

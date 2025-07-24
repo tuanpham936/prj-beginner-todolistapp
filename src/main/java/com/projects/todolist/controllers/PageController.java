1047ca6d0ac22082554bf56992d5438675a67d21
tuanpham936 <tuan.phamminh936@gmail.com>
2025-07-24 09:43:57 +0700

    remove: Search Bar

--- src/main/java/com/projects/todolist/controllers/PageController.java
+++ src/main/java/com/projects/todolist/controllers/PageController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-
 @Controller
 public class PageController {
     private final TaskRestController taskRestController;
@@ -17,16 +14,10 @@ public class PageController {
 
     @GetMapping("/")
     public String getHomePage(@RequestAttribute(required = false) String activeTab, Model page) {
-        page.addAttribute("activeTab", "todo");
+        page.addAttribute("activeTab", activeTab == null ? "todo" : activeTab);
         page.addAttribute("todo", taskRestController.getAllToDo());
         page.addAttribute("done", taskRestController.getAllDoneTasks());
         page.addAttribute("cancel", taskRestController.getAllCancelTasks());
-        return "home.html";
-    }
-
-    @PostMapping("/search")
-    public String search(@RequestParam(required = true) String activeTab, @RequestParam(required = true) String keyword, Model page) {
-        page.addAttribute("activeTab", activeTab);
         return "home.html";
     }
 }
